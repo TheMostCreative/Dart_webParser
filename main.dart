6e754ca41fd29e6fa83e5b0445554872db2c272b
Dmitriy Kolesnichenko <dimika2010@gmail.com>
2019-07-19 21:41:08 +0300

    First commit

---
+++ main.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:html/parser.dart';
+import 'package:html/dom.dart';
+import 'dart:io';
+
+main() async {
+  var jsonFile = File('data.json');
+  var sink = jsonFile.openWrite();
+  var result;
+  var client = Client();
+  Response response = await client.get('https://firebase.google.com/products/#develop-products'); //выбираем сайт
+
+  var document = parse(response.body); //Используем парсер
+  List<Element> links =
+      document.body.querySelectorAll('a[data-category="product-grid"]');
+
+  List<Map<String, dynamic>> linkMap = [];
+
+  for (var link in links) {
+    linkMap.add({
+      'title': link.text,
+    });
+  }
+  print(json.encode(linkMap)); //делаем енкод в JSOn
+
+  sink.write(json.encode(linkMap));
+  await sink.flush();
+  await sink.close();
+}
